306951f285e3f48ea01114221bd01ad90c95cde3
jongbaekKim <skyjbkim88@gmail.com>
2021-12-27 15:48:45 +0900

    27일 6교시 백업

---
+++ src/main/java/org/conan/controller/SampleController.java
@@ -0,0 +1,89 @@
+package org.conan.controller;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.conan.domain.SampleDTO;
+import org.conan.domain.SampleDTOList;
+import org.conan.domain.TodoDTO;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import lombok.extern.log4j.Log4j;
+
+@RequestMapping("/sample/*")
+@Controller
+@Log4j
+public class SampleController {
+	@GetMapping("/ex01")
+	public String ex01(SampleDTO dto) {
+		log.info("" + dto);
+		return "ex01";
+	}
+
+	@GetMapping("/ex02")
+	public String ex02List(@RequestParam("name") String name, @RequestParam("age") int age) {
+		log.info("name : " + name);
+		log.info("age : " + age);
+		return "ex02";
+	}
+
+	@GetMapping("/ex02List")
+	public String ex02List(@RequestParam("ids") ArrayList<String> ids) {
+		log.info("ids : " + ids);
+		return "ex02List";
+	}
+
+	@GetMapping("/ex02Array")
+	public String ex02Array(@RequestParam("ids") String[] ids) {
+		log.info("array ids : " + Arrays.toString(ids));
+		return "ex02List";
+	}
+
+	@GetMapping("/ex02Bean")
+	public String ex02Bean(SampleDTOList list) {
+		log.info("list dtos : " + list);
+		return "ex02Bean";
+	}
+
+	@InitBinder
+	public void InitBinder(WebDataBinder binder) {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		binder.registerCustomEditor(java.util.Date.class, new CustomDateEditor(dateFormat, false));
+	}
+
+	@GetMapping("/ex03")
+	public String ex03(TodoDTO todo) {
+		log.info("todo : " + todo);
+		return "ex03";
+	}
+
+	@GetMapping("/ex04")
+	public String ex04(SampleDTO dto, @ModelAttribute("page") int page) {
+		log.info("dto : " + dto);
+		log.info("page" + page);
+		return "/sample/ex04";
+	}
+
+	@GetMapping("/ex05")
+	public void ex05() {
+		log.info("ex05..................");
+	}
+
+	@GetMapping("/ex06")
+	public @ResponseBody SampleDTO ex06() {
+		log.info("ex06..................");
+		SampleDTO dto = new SampleDTO();
+		dto.setAge(10);
+		dto.setName("코난");
+		return dto;
+	}
+}
