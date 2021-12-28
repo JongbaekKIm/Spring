0857a392072b4eb448189f34fdc536c80e8efc28
jongbaekKim <skyjbkim88@gmail.com>
2021-12-28 10:39:46 +0900

    Board sql 기능 추가

--- src/main/java/org/conan/mapper/BoardMapper.java
+++ src/main/java/org/conan/mapper/BoardMapper.java
@@ -2,10 +2,18 @@ package org.conan.mapper;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Select;
 import org.conan.domain.BoardVO;
 
 public interface BoardMapper {
 	/* @Select("select * from tbl_board where BNO>0") */
-public List<BoardVO> getList();
+	public List<BoardVO> getList();
+
+	public void insert(BoardVO board);
+
+	public BoardVO read(long bno);
+
+	public int delete(long bno);
+
+	public int update(BoardVO board);
+
 }
