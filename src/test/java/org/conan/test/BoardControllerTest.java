56d843f43f17bbf9706eeec0774a1110a686d3cc
jongbaekKim <skyjbkim88@gmail.com>
2022-01-03 10:43:12 +0900

    댓글 처리를 위한 VO 생성 및 테스트 확인

--- src/test/java/org/conan/test/BoardControllerTest.java
+++ src/test/java/org/conan/test/BoardControllerTest.java
@@ -1,16 +1,22 @@
 package org.conan.test;
 
+import org.conan.domain.Ticket;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import com.google.gson.Gson;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
@@ -66,12 +72,22 @@ public class BoardControllerTest {
 				.getModelAndView().getViewName();
 		log.info(resultPage);
 	}
-	
+
 	@Test
-	public void testListPaging() throws Exception{
-		log.info(mockMvc.perform(MockMvcRequestBuilders.get("/board/list")
-				.param("pageNum", "2")
-				.param("amount", "3"))
+	public void testListPaging() throws Exception {
+		log.info(mockMvc.perform(MockMvcRequestBuilders.get("/board/list").param("pageNum", "2").param("amount", "3"))
 				.andReturn().getModelAndView().getModelMap());
+	}
+
+	@Test
+	public void testConvert() throws Exception {
+		Ticket ticket = new Ticket();
+		ticket.setTno(123);
+		ticket.setOwner("admin");
+		ticket.setGrade("SSS");
+		String jsonStr = new Gson().toJson(ticket);
+		log.info(jsonStr);
+		mockMvc.perform(post("/rsample/ticket").contentType(MediaType.APPLICATION_JSON).content(jsonStr))
+				.andExpect(status().is(200));
 	}
 }
